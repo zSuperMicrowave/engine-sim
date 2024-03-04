5addc2dbb6600faee00422081c6a61fd6414efe4
zSuperMicrowave_ <massimilianodss@outlook.it>
2024-03-04 21:21:56 +0100

    CilindroAudio full funzionante pt.1

--- Sistema audio/CilindroAudio.gd
+++ Sistema audio/CilindroAudio.gd
@@ -5,6 +5,7 @@ var coda_passaggi : Array[float]
 var puntatore_coda_precedente : float
 var puntatore_coda : float
 
+@export_range(0.1,4.0) var qualita := 1.0
 @export_range(0.01,2.0) var moltiplicatore_input_output := 1.0
 
 @export_category("ComponenteAudio")
@@ -24,6 +25,12 @@ var velocita_attraversamento := 1.0
 @export_group("Debug e Test")
 @export var silenzia_errori := false
 
+var ttt := 0.0
+func _process(delta):
+	ttt += delta
+	if ttt >= 5.0 && ttt < 5.1 : 
+		max_att = 0.0
+		print(dimensione_coda)
 
 func _ready():
 	#Squenza di test: eliminami quando finisci
@@ -82,7 +89,7 @@ func _enter_tree():
 
 
 func ottieni_campione() -> float:
-	return 0.0
+	#return 0.0
 	aggiorna_riverbero()
 
 	var delta = 1.0 / InfoAudio.frequenza_campionamento_hz
@@ -119,57 +126,49 @@ func avanza_puntatori():
 
 
 func applica_attenuazione(attenuazione : float):
-	pass
-#	for i in range(velocita_attraversamento_int+1) :
-#		var nuovo_i_negativo = fmod(i_buffer_negativo + i,dimensione_coda)
-#		var nuovo_i_positivo = dimensione_coda - 1 - nuovo_i_negativo
-#
-#
-#		if i == velocita_attraversamento_int :
-#			direzione_negativa_passaggi[nuovo_i_negativo] *=\
-#				lerpf(attenuazione,1.0,resto_puntatori)
-#			direzione_positiva_passaggi[nuovo_i_positivo] *=\
-#				lerpf(attenuazione,1.0,resto_puntatori)
-#		else :
-#			direzione_negativa_passaggi[nuovo_i_negativo] *= attenuazione
-#			direzione_positiva_passaggi[nuovo_i_positivo] *= attenuazione
+	for i in range(floori(puntatore_coda-velocita_attraversamento), ceili(puntatore_coda)) :
+		var t : float = 1 - min((puntatore_coda - i) / velocita_attraversamento, 1.0)
+		i = fposmod(i, dimensione_coda) as int
+		
+		var att = lerpf(1.0,attenuazione,t)
+		
+		coda_passaggi[dimensione_coda + i] *= att
+		coda_passaggi[i] *= att
+	
+	for i in range(ceili(puntatore_coda), ceili(puntatore_coda + velocita_attraversamento)) :
+		var t : float = min((i - puntatore_coda) / velocita_attraversamento, 1.0)
+		i = fposmod(i, dimensione_coda) as int
+		
+		var att = lerpf(1.0,attenuazione,t)
+		
+		coda_passaggi[dimensione_coda + i] *= att
+		coda_passaggi[i] *= att
 
 
 func scambia_valori():
-	pass
-#	for i in range(velocita_attraversamento_int+1) :
-#		var nuovo_i_negativo = fmod(i_buffer_negativo + i,dimensione_coda)
-#		var nuovo_i_positivo = dimensione_coda - 1 - nuovo_i_negativo
-#
-#
-#		if i == velocita_attraversamento_int :
-#			var temp_neg = direzione_negativa_passaggi[nuovo_i_negativo]
-#
-#			direzione_negativa_passaggi[nuovo_i_negativo] = lerpf(
-#				direzione_negativa_passaggi[nuovo_i_negativo],
-#				direzione_positiva_passaggi[nuovo_i_positivo] * moltiplicatore_energia_rimbalzo,
-#				resto_puntatori)
-#
-#			if tubo_chiuso :
-#				direzione_negativa_passaggi[nuovo_i_negativo] = lerpf(
-#					direzione_negativa_passaggi[nuovo_i_negativo],
-#					direzione_negativa_passaggi[nuovo_i_negativo] * -1,
-#					resto_puntatori)
-#
-#			direzione_positiva_passaggi[nuovo_i_positivo] = lerpf(
-#				direzione_positiva_passaggi[nuovo_i_positivo],
-#				temp_neg * moltiplicatore_energia_rimbalzo,
-#				resto_puntatori)
-#		else :
-#			var temp_neg = direzione_negativa_passaggi[nuovo_i_negativo]
-#
-#			direzione_negativa_passaggi[nuovo_i_negativo] =\
-#				 direzione_positiva_passaggi[nuovo_i_positivo] * moltiplicatore_energia_rimbalzo
-#
-#			if tubo_chiuso : direzione_negativa_passaggi[nuovo_i_negativo] *= -1
-#
-#			direzione_positiva_passaggi[nuovo_i_positivo] =\
-#				 temp_neg * moltiplicatore_energia_rimbalzo
+	for i in range(floori(puntatore_coda-velocita_attraversamento), ceili(puntatore_coda)) :
+		var t : float = 1 - min((puntatore_coda - i) / velocita_attraversamento, 1.0)
+		i = fposmod(i, dimensione_coda) as int
+		
+		# Effettua scambio
+		var temp = coda_passaggi[dimensione_coda + i]
+		coda_passaggi[dimensione_coda + i] *= (1-t)
+		coda_passaggi[dimensione_coda + i] += coda_passaggi[i] * t * moltiplicatore_energia_rimbalzo
+		
+		coda_passaggi[i] *= (1-t)
+		coda_passaggi[i] += temp * t * moltiplicatore_energia_rimbalzo
+	
+	for i in range(ceili(puntatore_coda), ceili(puntatore_coda + velocita_attraversamento)) :
+		var t : float = min((i - puntatore_coda) / velocita_attraversamento, 1.0)
+		i = fposmod(i, dimensione_coda) as int
+		
+		# Effettua scambio
+		var temp = coda_passaggi[dimensione_coda + i]
+		coda_passaggi[dimensione_coda + i] *= (1-t)
+		coda_passaggi[dimensione_coda + i] += coda_passaggi[i] * t * moltiplicatore_energia_rimbalzo
+		
+		coda_passaggi[i] *= (1-t)
+		coda_passaggi[i] += temp * t * moltiplicatore_energia_rimbalzo
 
 var ultimo_input := 0.0
 func input_valori(input : float):
@@ -189,12 +188,24 @@ func input_valori(input : float):
 	ultimo_input = input
 
 
+var min_att = 13980513095.0
+var max_att = 0.0
+
 func aggiorna_riverbero():
 	var nuovo_riverbero : int = componente_precedente.ottieni_riverbero()
+
+	nuovo_riverbero = clamp(nuovo_riverbero*qualita,10,2000)
 
 	if nuovo_riverbero > dimensione_coda :
 		dimensione_coda = nuovo_riverbero
 		coda_passaggi.resize(dimensione_coda*2)
 		puntatore_coda = fmod(puntatore_coda,dimensione_coda)
 
-	velocita_attraversamento = dimensione_coda as float / nuovo_riverbero as float
+	velocita_attraversamento = dimensione_coda*qualita as float / nuovo_riverbero as float
+
+	if min_att > velocita_attraversamento :
+		min_att = velocita_attraversamento
+		print("MIN ATT = ",velocita_attraversamento)
+	if max_att < velocita_attraversamento :
+		max_att = velocita_attraversamento
+		print("MAX ATT = ",velocita_attraversamento)
