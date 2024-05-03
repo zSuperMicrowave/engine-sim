62e968ac3e16275faf7d57aaca868100abf11505
zSuperMicrowave_ <massimilianodss@outlook.it>
2024-05-03 18:09:02 +0200

    UI realizzata

---
+++ main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var c1 : Control
+@export var c2 : Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func get_pressure(container : Control):
+	return c1.
