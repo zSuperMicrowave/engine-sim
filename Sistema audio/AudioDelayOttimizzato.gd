2e4aaf9108f6cb51e1c27d463047d687ed799748
zSuperMicrowave_ <massimilianodss@outlook.it>
2024-05-09 16:37:46 +0200

    Implementa #36, buffer ciclico implementato in delayottimizzato

--- Sistema audio/AudioDelayOttimizzato.gd
+++ Sistema audio/AudioDelayOttimizzato.gd
@@ -218,3 +218,26 @@ func ridimensiona_array(arr : Array[float], nuova_grandezza : int):
 	arr = nuovo_buffer
 	
 	return arr
+
+func ridimensiona_array_ciclico(arr : Array[float], nuova_grandezza : int) -> Array[float]:
+	var passo = nuova_grandezza as float / arr.size()
+	var nuovo_buffer : Array[float]
+	nuovo_buffer.resize(nuova_grandezza)
+	
+	if nuova_grandezza > arr.size():
+		for i in range(nuova_grandezza):
+			var idx = i / passo
+			var flr = int(idx) % arr.size()
+			var cel = (flr + 1) % arr.size()
+			var t = idx - flr
+			nuovo_buffer[int(i+passo*0.45) % nuova_grandezza] = lerp(arr[flr], arr[cel], t)
+	else:
+		for i in range(nuova_grandezza):
+			var start_idx = int(i * (arr.size() / nuova_grandezza))
+			var end_idx = int((i + 1) * (arr.size() / nuova_grandezza))
+			var sum = 0.0
+			for j in range(start_idx, end_idx):
+				sum += arr[j % arr.size()]
+			nuovo_buffer[i] = sum / (end_idx - start_idx)
+
+	return nuovo_buffer
--- esperimenti/dagadg.gd
+++ esperimenti/dagadg.gd
@@ -1,45 +1,30 @@
 extends Node3D
 
-
-
 func _ready():
-	var nuova_grandezza := 3
-	var arr := [1,2,3,4,5,6,7,8]
+	var nuova_grandezza = 16
+	var arr : Array[float] = [-8, 4, 6, 8]
 	print(arr)
-	
-	
-	
-	var passo := (nuova_grandezza-1) as float / (arr.size()-1) as float
+	print(ridimensiona_array(arr, nuova_grandezza))
+
+func ridimensiona_array(arr : Array[float], nuova_grandezza : int) -> Array[float]:
+	var passo = nuova_grandezza as float / arr.size()
 	var nuovo_buffer : Array[float]
 	nuovo_buffer.resize(nuova_grandezza)
-	nuovo_buffer.fill(0.0)
-
-	if nuova_grandezza > arr.size() :
-		for i in range(nuova_grandezza):
-			var flr := clampi(floori(i / passo), 0, arr.size()-1)
-			var cel := clampi(ceili( i / passo), 0, arr.size()-1)
-			print(flr)
-			
-			nuovo_buffer[i] = lerpf(arr[flr],arr[cel], i/passo - flr)
-	else :
-		var div : Array[float]
-		div.resize(nuova_grandezza)
-		div.fill(0.0)
-		for i in range(arr.size()):
-			var flr := clampi(floori(i * passo), 0, arr.size()-1)
-			var cel := clampi(ceili( i * passo), 0, arr.size()-1)
-			print(flr)
-			
-			nuovo_buffer[flr] += arr[i] * maxf(i*passo - flr, 0.0)
-			nuovo_buffer[cel] += arr[i] * maxf(cel - i*passo, 0.0)
-			
-			div[flr] += maxf(i*passo - flr, 0.0)
-			div[cel] += maxf(cel - i*passo, 0.0)
-		
-		for i in range(nuova_grandezza) :
-			nuovo_buffer[i] /= div[i]
-
-
-	arr = nuovo_buffer
 	
-	print(arr)
+	if nuova_grandezza > arr.size():
+		for i in range(nuova_grandezza):
+			var idx = i / passo
+			var flr = int(idx) % arr.size()
+			var cel = (flr + 1) % arr.size()
+			var t = idx - flr
+			nuovo_buffer[int(i+passo*0.45) % nuova_grandezza] = lerp(arr[flr], arr[cel], t)
+	else:
+		for i in range(nuova_grandezza):
+			var start_idx = int(i * (arr.size() / nuova_grandezza))
+			var end_idx = int((i + 1) * (arr.size() / nuova_grandezza))
+			var sum = 0.0
+			for j in range(start_idx, end_idx):
+				sum += arr[j % arr.size()]
+			nuovo_buffer[i] = sum / (end_idx - start_idx)
+
+	return nuovo_buffer
