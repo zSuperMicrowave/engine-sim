7ef9b5641b9d2ddf9fc0bccd2211148bcdf6463c
zSuperMicrowave_ <massimilianodss@outlook.it>
2024-05-09 17:33:38 +0200

    Aggiunto DC offset automatico nel riproduttore audio 3d

--- Sistema audio/RiproduttoreAudio3D.gd
+++ Sistema audio/RiproduttoreAudio3D.gd
@@ -2,7 +2,18 @@ extends AudioStreamPlayer3D
 class_name RiproduttoreAudio3D
 
 @export var componente_audio_precedente : ComponenteAudio
-@export_range(-1.0,1.0) var dc_offset_manuale := 0.0
+@export_range(0.0,2.0) var qnt_dc_offset := 0.9
+var dc_offset_continuo := 0.0
+var dc_offset := 0.1
+var vel_agg_dc_offset := 0.0
+@export_range(1,8000) var lentezza_aggiornamento_dc_offset := 100 :
+	set(val):
+		lentezza_aggiornamento_dc_offset = val
+		vel_agg_dc_offset = 1.0 / lentezza_aggiornamento_dc_offset as float
+	get:
+		return lentezza_aggiornamento_dc_offset
+
+
 var playback : AudioStreamGeneratorPlayback
 
 func _ready():
@@ -21,11 +32,21 @@ func _physics_process(delta):
 		print("/!\\AUDIO BLOCCATO/!\\")
 	_elabora_frame_audio()
 
-
+var count : int = 0
 func _elabora_frame_audio():
 	var frame_rimanenti := playback.get_frames_available()
 	while frame_rimanenti > 0:
-		var val = componente_audio_precedente.ottieni_campione() + dc_offset_manuale
+		var val = componente_audio_precedente.ottieni_campione()
+		
+		dc_offset_continuo += val*vel_agg_dc_offset
+		count += 1
+		if count > lentezza_aggiornamento_dc_offset:
+			dc_offset = dc_offset_continuo
+			dc_offset_continuo = 0
+			count = 0
+		
+		val -= (dc_offset * 0.5)*qnt_dc_offset
+		
 		if is_nan(val) : print("/!\\VALORE NAN RESTITUITO/!\\")
 		playback.push_frame(Vector2.ONE * val)
 		frame_rimanenti -= 1
