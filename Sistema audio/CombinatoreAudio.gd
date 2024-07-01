a69991c50c47a2a67d412465f98ff95ecde4cc2e
Microwave <MassimilianoDSS@Outlook.it>
2024-07-01 02:18:00 +0200

    Aggiunto multithreading per il combinatore audio

--- Sistema audio/CombinatoreAudio.gd
+++ Sistema audio/CombinatoreAudio.gd
@@ -3,18 +3,24 @@ class_name CombinatoreAudio
 
 @export var componenti_precedenti : Array[ComponenteAudio]
 @export var esegui_media := false
+var threads : Array[Thread] = []
 
+func _enter_tree():
+	for c in componenti_precedenti:
+		threads.append(Thread.new())
 
 func sample_audio(samps : int) -> Array[float]:
 	var out : Array[float] = []
 	for i in range(samps) :
 		out.append(0.0)
 
-	for c in componenti_precedenti :
-		var samps_buf := c.sample_audio(samps)
-		
-		for i in range(samps) :
-			out[i] += samps_buf[i]
+	for i in range(componenti_precedenti.size()) :
+		threads[i].start(Callable(componenti_precedenti[i],"sample_audio").bind(samps))
+	
+	for i in range(componenti_precedenti.size()) :
+		var samps_buf : Array[float] = threads[i].wait_to_finish()
+		for j in range(samps) :
+			out[j] += samps_buf[j]
 	
 	if esegui_media :
 		for i in range(samps) :
